03082e7196fd06e9ca5ba786cb34ee2a4e4cf66a
iamshiv007 <iamshiv20032003@gmail.com>
2023-05-26 20:12:21 +0530

    Reset _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,5 @@
-import Head from 'next/head'
-import { ChakraProvider } from '@chakra-ui/react'
-import Layout from '@/components/Layout'
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return(
-  <>
-  <Head>
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
-    </Head>
-    <ChakraProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
-  </>)
+  return <Component {...pageProps} />
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
-import Image from 'next/image'
 
 export default function Home() {
   return (
     <>
+      <Navbar />
+      <Footer />
     </>
   )
 }
